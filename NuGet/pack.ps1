b5f6755ee19c243a11504f26cb91a2cff78d2f19
Josh Keegan <josh.keegan@gmx.com>
2015-03-09 21:39:52 +0000

    nuget (missing files)

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\KLogNet\KLogNet\bin\Release\KLogNet.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\KLogNet.nuspec) 
+$content = $content -replace '\$version\$', $versionStr
+
+$content | Out-File $root\NuGet\KLogNet.compiled.nuspec
+
+& nuget pack $root\NuGet\KLogNet.compiled.nuspec
