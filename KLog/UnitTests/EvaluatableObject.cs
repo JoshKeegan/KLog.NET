3669e158638a2f9df44186ca6572f14257fa60c0
Josh Keegan <josh.keegan@gmx.com>
2015-03-11 20:33:20 +0000

    evaluation tests

---
+++ KLog/UnitTests/EvaluatableObject.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * KLog.NET Unit Tests
+ * Evaluatable Object - an object that stores whether its ToString() method gets evaluated
+ * Authors:
+ * Josh Keegan 11/03/2015
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests
+{
+    internal class EvaluatableObject
+    {
+        internal bool Evaluated { get; private set; }
+
+        public EvaluatableObject()
+        {
+            this.Evaluated = false;
+        }
+
+        public override string ToString()
+        {
+            this.Evaluated = true;
+            return "A wild Evaluatable Object appeared!";
+        }
+    }
+}
---
+++ KLog/UnitTests/LogTests.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * KLog.NET Unit Tests
+ * Log Tests
+ * Authors:
+ *  Josh Keegan 11/03/2015
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using KLog;
+
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LogTests
+    {
+        [Test]
+        public void TestObjectEvaluation()
+        {
+            //An object should only be evaluated if it is actually going to be written out
+            EvaluatableObject obj = new EvaluatableObject();
+            Log log = new ConsoleLog(LogLevel.All);
+
+            Assert.IsFalse(obj.Evaluated);
+
+            log.Error("Oh hi there {0}", obj);
+            Assert.IsTrue(obj.Evaluated);
+        }
+
+        [Test]
+        public void TestObjectEvaluationNotEvaluated()
+        {
+            //An object should not be evaluated if it isn't written out
+            EvaluatableObject obj = new EvaluatableObject();
+            Log log = new ConsoleLog(LogLevel.None);
+
+            Assert.IsFalse(obj.Evaluated);
+
+            log.Info("Sup {0}", obj);
+            Assert.IsFalse(obj.Evaluated);
+        }
+    }
+}
