042ccc9863fa02d531816a55573cfadda61fa3f6
Josh Keegan <josh.keegan@gmx.com>
2015-03-11 19:59:16 +0000

    Only package for nuget if on the release branch

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,12 +1,19 @@
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\KLog\KLog\bin\Release\KLog.dll").GetName().Version
-$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
+if($env:APPVEYOR_REPO_BRANCH -eq "release")
+{
+	$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+	$version = [System.Reflection.Assembly]::LoadFile("$root\KLog\KLog\bin\Release\KLog.dll").GetName().Version
+	$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
 
-Write-Host "Setting .nuspec version tag to $versionStr"
+	Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\NuGet\KLog.nuspec) 
-$content = $content -replace '\$version\$', $versionStr
+	$content = (Get-Content $root\NuGet\KLog.nuspec) 
+	$content = $content -replace '\$version\$', $versionStr
 
-$content | Out-File $root\NuGet\KLog.compiled.nuspec
+	$content | Out-File $root\NuGet\KLog.compiled.nuspec
 
-& nuget pack $root\NuGet\KLog.compiled.nuspec
+	& nuget pack $root\NuGet\KLog.compiled.nuspec
+}
+else
+{
+	Write-Host "Not packaging for nuget. To package & push to nuget, merge onto the release branch"
+}
