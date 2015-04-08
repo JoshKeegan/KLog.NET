88e5cef5079729deda5088b7f2f4abbe60279a2f
Josh Keegan <josh@visav.co.uk>
2015-04-08 15:45:43 +0100

    Log rate limiter

---
+++ KLog/KLog/LogRateLimiter.cs
@@ -0,0 +1,173 @@
+﻿/*
+ * KLog.NET
+ * LogRateLimiter - Implementation of Log that wraps another log. 
+ *  Limits the number of entries that can get logged in a given time period.
+ *  If the limit is exceeded, no log messages will get written until it dips back below the threshold
+ * Authors:
+ *  Josh Keegan 08/04/2015
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KLog
+{
+    public class LogRateLimiter<T> : Log, IDisposable where T : Log
+    {
+        #region Private Variables
+
+        private T underlyingLog;
+
+        //TODO: Could alter queue to be lock-free and fixed length (combination of MS ConcurrentQueue & PiSearch FixedLengthQueue)
+        //  if ever requiure that level of performance
+        private Queue<DateTime> logTimes;
+        private object logTimesLock = new object();
+        private TimeSpan timeSpan;
+        private int numEntries;
+        private bool inRateLimit = false;
+        private HandleEnterExitRateLimit onEnterRateLimit;
+        private HandleEnterExitRateLimit onExitRateLimit;
+
+        #endregion
+
+        #region Log Implementation
+
+        protected override void write(LogEntry entry)
+        {
+            //Update logTimes
+            bool doWrite = true;
+            bool enteredRateLimit = false;
+            bool exitedRateLimit = false;
+
+            lock(logTimesLock)
+            {
+                logTimes.Enqueue(entry.EventDate);
+
+                //If we've written numEntries to the log, check if we're within the rate limit
+                if(logTimes.Count > numEntries)
+                {
+                    DateTime oldest = logTimes.Dequeue();
+                    TimeSpan loggedAgo = entry.EventDate - oldest;
+
+                    //If we have logged numEntries items in less time than the rate limit
+                    if(loggedAgo < timeSpan)
+                    {
+                        //Don't write this entry to the log
+                        doWrite = false;
+
+                        //If we weren't already in the rate limit, we just entered it
+                        if(!inRateLimit)
+                        {
+                            inRateLimit = true;
+                            enteredRateLimit = true;
+                        }
+                    }
+                    //Otherwise, if we were in the rate limit we aren't any more
+                    else if(inRateLimit)
+                    {
+                        inRateLimit = false;
+                        exitedRateLimit = true;
+                    }
+                }
+            }
+
+            if(doWrite)
+            {
+                underlyingLog.internalWrite(entry);
+            }
+            
+            if(enteredRateLimit)
+            {
+                onEnterRateLimit(entry);
+            }
+
+            if(exitedRateLimit)
+            {
+                onExitRateLimit(entry);
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LogRateLimiter(T log, TimeSpan timeSpan, int numEntries,
+            HandleEnterExitRateLimit onEnterRateLimit, HandleEnterExitRateLimit onExitRateLimit)
+            : base(log.logLevel)
+        {
+            //Validation
+            if(log == null)
+            {
+                throw new ArgumentNullException("log");
+            }
+            if(timeSpan == null)
+            {
+                throw new ArgumentNullException("timeSpan");
+            }
+            if(numEntries < 0)
+            {
+                throw new ArgumentOutOfRangeException("numEntries must be >= 0");
+            }
+            if(onEnterRateLimit == null)
+            {
+                throw new ArgumentNullException("onEnterRateLimit");
+            }
+            if (onExitRateLimit == null)
+            {
+                throw new ArgumentNullException("onExitRateLimit");
+            }
+            this.underlyingLog = log;
+
+            this.logTimes = new Queue<DateTime>();
+            this.timeSpan = timeSpan;
+            this.numEntries = numEntries;
+            this.onEnterRateLimit = onEnterRateLimit;
+            this.onExitRateLimit = onExitRateLimit;
+        }
+
+        #endregion
+
+        #region Public Delegates
+
+        public delegate void HandleEnterExitRateLimit(LogEntry entry);
+
+        #endregion
+
+        #region Implement IDisposable
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        ~LogRateLimiter()
+        {
+            this.Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            //Write all of the log entries out before letting the log be disposed of
+            BlockWhileWriting();
+
+            //Callling Dispose(): free managed resources
+            if(disposing)
+            {
+                //If there is an underlying log that is disposable, dispose of it now
+                if(underlyingLog != null
+                    && typeof(IDisposable).IsAssignableFrom(typeof(T)))
+                {
+                    IDisposable disposable = (IDisposable)underlyingLog;
+                    disposable.Dispose();
+                }
+            }
+
+            //Dispose or finalizer, free any native resources
+        }
+
+        #endregion
+    }
+}
---
+++ KLog/UnitTests/LogRateLimiterTests.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * KLog.NET Unit Tests
+ * LogRateLimiter Tests
+ * Authors:
+ *  Josh Keegan 08/04/2015
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using KLog;
+using KLog.Programmatic;
+
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LogRateLimiterTests
+    {
+        [Test]
+        public void TestWrite()
+        {
+            StackLog log = new StackLog(KLog.LogLevel.All);
+            LogRateLimiter<StackLog> rateLimiter = new LogRateLimiter<StackLog>(log, new TimeSpan(1, 0, 0), 100, 
+                (entry) => { }, (entry) => { });
+
+            rateLimiter.Debug("test message");
+
+            Assert.AreEqual("test message", log.Stack.Pop().Message);
+        }
+
+        [Test]
+        public void TestExceedRateLimitDoesntWrite()
+        {
+            //Write 110 entries with a limit of 100. Check that the last one written is #100
+            StackLog log = new StackLog(KLog.LogLevel.All);
+            LogRateLimiter<StackLog> rateLimiter = new LogRateLimiter<StackLog>(log, new TimeSpan(1, 0, 0), 100,
+                (entry) => { }, (entry) => { });
+
+            for(int i = 0; i < 110; i++)
+            {
+                rateLimiter.Debug("test message " + i);
+            }
+
+            Assert.AreEqual("test message 99", log.Stack.Pop().Message);
+        }
+        
+        [Test]
+        public void TestExceedRateLimitAndThenWait()
+        {
+            //Set rate limit to re1 message per 10ms
+            StackLog log = new StackLog(KLog.LogLevel.All);
+            LogRateLimiter<StackLog> rateLimiter = new LogRateLimiter<StackLog>(log, new TimeSpan(0, 0, 0, 0, 5), 1,
+                (entry) => { }, (entry) => { });
+
+            rateLimiter.Debug("1");
+            rateLimiter.Debug("2");
+            Thread.Sleep(5);
+            rateLimiter.Debug("3");
+
+            //Only 3 & 1 should have been logged
+            Assert.AreEqual("3", log.Stack.Pop().Message);
+            Assert.AreEqual("1", log.Stack.Pop().Message);
+        }
+
+        [Test]
+        public void TestExceedRateLimitRunsHandler()
+        {
+            //Set rate limit to re1 message per 10ms
+            StackLog log = new StackLog(KLog.LogLevel.All);
+            string exceededOn = null;
+            LogRateLimiter<StackLog> rateLimiter = new LogRateLimiter<StackLog>(log, new TimeSpan(1, 0, 0), 1,
+                (entry) => 
+                {
+                    exceededOn = entry.Message;
+                }, (entry) => { });
+
+            rateLimiter.Debug("blah");
+            rateLimiter.Debug("too much");
+
+            Assert.AreEqual("blah", log.Stack.Pop().Message);
+            Assert.AreEqual("too much", exceededOn);
+        }
+
+        [Test]
+        public void TestEnterExitRateLimitRunsHandler()
+        {
+            //Set rate limit to re1 message per 10ms
+            StackLog log = new StackLog(KLog.LogLevel.All);
+            string exceededOn = null;
+            string exitedOn = null;
+            LogRateLimiter<StackLog> rateLimiter = new LogRateLimiter<StackLog>(log, new TimeSpan(0, 0, 0, 0, 5), 1,
+                (entry) =>
+                {
+                    exceededOn = entry.Message;
+                }, 
+                (entry) => 
+                {
+                    exitedOn = entry.Message;
+                });
+
+            rateLimiter.Debug("blah");
+            rateLimiter.Debug("too much");
+            rateLimiter.Debug("ignore me");
+
+            Thread.Sleep(5);
+            rateLimiter.Debug("back in the game");
+
+            Assert.AreEqual("back in the game", log.Stack.Pop().Message);
+            Assert.AreEqual("blah", log.Stack.Pop().Message);
+            Assert.AreEqual("too much", exceededOn);
+            Assert.AreEqual("back in the game", exitedOn);
+        }
+    }
+}
