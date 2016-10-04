ac03a0f473f526cf532faa33fead32cb5858ea5a
Josh Keegan <josh@visav.co.uk>
2016-10-04 14:17:16 +0100

    QueueLog

---
+++ KLog/KLog/Programmatic/QueueLog.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * KLog.NET
+ * QueueLog - Implementation of a Log that can have its messages queried programatically as a Queue
+ * Authors:
+ *  Josh Keegan 04/10/2016
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLog.Programmatic
+{
+    public class QueueLog : Log
+    {
+        // Public Variables
+        public readonly Queue<LogEntry> Queue = new Queue<LogEntry>();
+
+        // Implement Log
+        protected override void write(LogEntry entry)
+        {
+            Queue.Enqueue(entry);
+        }
+
+        // Constructors
+        public QueueLog(LogLevel logLevel)
+            : base(logLevel) {  }
+    }
+}
