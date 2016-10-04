1293092fe7fdf3de1af6081ed2e85f97a8c806d3
Josh Keegan <josh@visav.co.uk>
2016-10-04 13:51:08 +0100

    Add a programmatic log using a LinkedList

---
+++ KLog/KLog/Programmatic/LinkedListLog.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * KLog.NET
+ * ListLog - Implementation of a Log that can have its messages queried programatically as a LinkedList
+ * Authors:
+ *  Josh Keegan 04/10/2016
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLog.Programmatic
+{
+    public class LinkedListLog : Log
+    {
+        // Public Variables
+        public readonly LinkedList<LogEntry> List = new LinkedList<LogEntry>();
+
+        // Implement Log
+        protected override void write(LogEntry entry)
+        {
+            List.AddLast(entry);
+        }
+
+        // Constructors
+        public LinkedListLog(LogLevel logLevel)
+            : base(logLevel) {  }
+    }
+}
