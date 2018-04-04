f8d65fe9867da36f18983fa1c3b89ac0f0f137e1
Josh Keegan <josh@visav.co.uk>
2018-04-04 15:44:45 +0100

    KLog.Web nuget - probably forgot loads of stuff and appveyor won't be configured correctly for multiple packages

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,8 +1,13 @@
 if($env:APPVEYOR_REPO_BRANCH -eq "release")
 {
 	$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+
+	# Main KLog lib
+	Write-Host "Preparing KLog.Net"
+	
 	$version = [System.Reflection.Assembly]::LoadFile("$root\KLog\KLog\bin\Release\KLog.dll").GetName().Version
 	$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
+	$klogVersionStr = $versionStr
 
 	Write-Host "Setting .nuspec version tag to $versionStr"
 
@@ -12,6 +17,22 @@ if($env:APPVEYOR_REPO_BRANCH -eq "release")
 	$content | Out-File $root\NuGet\KLog.compiled.nuspec
 
 	& nuget pack $root\NuGet\KLog.compiled.nuspec
+	
+	# KLog.Web lib
+	Write-Host "Preparing KLog.Web"
+	
+	$version = [System.Reflection.Assembly]::LoadFile("$root\KLog\KLog.Web\bin\Release\KLog.Web.dll").GetName().Version
+	$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
+
+	Write-Host "Setting .nuspec version tag to $versionStr"
+
+	$content = (Get-Content $root\NuGet\KLog.Web.nuspec) 
+	$content = $content -replace '\$version\$', $versionStr
+	$content = $content -replace '\$klogVersion\$', $klogVersionStr
+
+	$content | Out-File $root\NuGet\KLog.Web.compiled.nuspec
+
+	& nuget pack $root\NuGet\KLog.Web.compiled.nuspec
 }
 else
 {
