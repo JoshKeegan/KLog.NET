410afcc4b2d85bff6867bf828006c10ce8b62892
Josh Keegan <josh@visav.co.uk>
2018-06-13 15:47:24 +0100

    Updates to nuget and appveyor setup for new .NET Standard build of KLog

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,22 +1,13 @@
 if($env:APPVEYOR_REPO_BRANCH -eq "release")
 {
 	$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-
-	# Main KLog lib
-	Write-Host "Preparing KLog.Net"
 	
-	$version = [System.Reflection.Assembly]::LoadFile("$root\KLog\KLog\bin\Release\KLog.dll").GetName().Version
+	$version = [System.Reflection.Assembly]::LoadFile("$root\KLog\KLog\bin\Release\netstandard2.0\KLog.dll").GetName().Version
 	$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
 	$klogVersionStr = $versionStr
-
-	Write-Host "Setting .nuspec version tag to $versionStr"
-
-	$content = (Get-Content $root\NuGet\KLog.nuspec) 
-	$content = $content -replace '\$version\$', $versionStr
-
-	$content | Out-File $root\NuGet\KLog.compiled.nuspec
-
-	& nuget pack $root\NuGet\KLog.compiled.nuspec
+	
+	#  Main KLog lib no longer packed here. NuGet package is now generated on build following
+	#	switch to .NET Standard. See project file for nuget settings.
 	
 	# KLog.Web lib
 	Write-Host "Preparing KLog.Web"
